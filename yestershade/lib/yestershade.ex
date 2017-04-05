a69c8de57c8816a09172e0e585d840833baf0cec
Daniel Seifert <das.mcs1998@gmail.com>
2017-04-04 22:58:34 -0700

    Initial Commit

---
+++ yestershade/lib/yestershade.ex
@@ -0,0 +1,18 @@
+defmodule Yestershade do
+  @moduledoc """
+  Documentation for Yestershade.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Yestershade.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ yestershade/test/yestershade_test.exs
@@ -0,0 +1,8 @@
+defmodule YestershadeTest do
+  use ExUnit.Case
+  doctest Yestershade
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
